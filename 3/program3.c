d1e5422ff01e57d5329a7dccc218f8c71e958c94
marcinmiszkurka <marcin.miszkurka@interia.pl>
2017-10-20 08:23:32 +0200

    nowy folder i podfoldery tj katalog czynnosci z dnia 20.10.2017

---
+++ 3/program3.c
@@ -0,0 +1,17 @@
+/* dolacz biblioteke stanbdardowa input-output*/
+#include <stdio.h>
+
+/* y=f(x) */ /* tam co funkcja w takich nawiasach okresla sie opisy*/
+/*  main()=> 0 */
+
+int dodaj(int a, int b) /* funkcja glowna programu, zwraaca wartosc całkowita*/
+{
+return a+b;
+}
+
+int main()
+{printf("%di/n , dodaj(4,5));
+
+	return 0; /*main zwraca wartosc 0*/
+
+}
